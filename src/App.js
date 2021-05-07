9a9397e6247d01fc10503180442f734652b21d1f
Vintha B <vintha@Shanthamurthys-MacBook-Pro.local>
2021-05-07 13:00:35 +0530

    main

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import Coursesales from './coursesales';
 
-function App() {
+class App extends Component {
+  render(){
+    var courses = [
+      { name: 'Complete iOS10 dev course', price: 199 },
+      { name: 'Complete pentesting course', price: 299 },
+      { name: 'Complete front end dev course', price: 180 },
+      { name: 'Bug Bounty and web app pentesting', price: 190 },
+
+    ];
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h2>Welcome to Course Purchase pages</h2>
+        
+      </div>
+      <Coursesales items={courses} />
     </div>
   );
+}
 }
 
 export default App;
